d4af41dc7acc4b597f92c8c5b6b0ce6c89b5790b
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-12-03 16:07:17 +0000

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var http = require('http');
-const person = require("./variable")
+const http = require('http')
+const path = require("path")
+const fs = require("fs")
 
-console.log(person)
 //create a server object:
-http.createServer(function (req, res) {
-  res.write(`${person}: fbi wanted`); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+http.createServer((req, res) => {
+  if(req.url === '/'){
+    fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+      if(err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.end(content)
+    })
+  }
+  // res.write(`${person.name}: fbi wanted`); //write a response to the client
+  // res.end(); //end the response
+}).listen(80, () => console.log("server listening on port 80")); //the server object listens on port 80
