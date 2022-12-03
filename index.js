f3eb50d2c4b529ff49ae8daeb8e5ca15bcd4f42a
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-12-03 16:41:17 +0000

    first commit

--- index.js
+++ index.js
@@ -2,15 +2,55 @@ const http = require('http')
 const path = require("path")
 const fs = require("fs")
 
-//create a server object:
+// ===== works for html only =====
+// http.createServer((req, res) => {
+//   if(req.url === '/'){
+//     fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+//       if(err) throw err;
+//       res.writeHead(200, {'Content-Type': 'text/html'})
+//       res.end(content)
+//     })
+//   }
+// }).listen(80, () => console.log("server listening on port 80"))
+
 http.createServer((req, res) => {
-  if(req.url === '/'){
-    fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
-      if(err) throw err;
-      res.writeHead(200, {'Content-Type': 'text/html'})
-      res.end(content)
-    })
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+  let extname = path.extname(filePath)
+  let contentType = "text/html"
+  switch(extname){
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.json':
+      contentType = 'application/json'
+      break
+    case '.png':
+      contentType = 'image/png'
+      break
+    case '.jpg':
+      contentType = 'image/jpg'
+      break
   }
-  // res.write(`${person.name}: fbi wanted`); //write a response to the client
-  // res.end(); //end the response
-}).listen(80, () => console.log("server listening on port 80")); //the server object listens on port 80
+  // read
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if(err.code == 'ENOENT'){
+        //page not found
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content)=> {
+          res.writeHead(200, {'Content-Type': 'text/html'})
+          res.end(content, 'utf8')
+        })
+      }else{
+        //server error
+        res.writeHead(500)
+        res.end('sever error')
+      }
+    }else{
+      res.writeHead(200, {'Content-Type': contentType})
+      res.end(content, 'utf8')
+    }
+  })
+}).listen(80, () => console.log("server listening on port 80"))
