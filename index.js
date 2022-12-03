db5af1d39268904c40ca9c0fc365063c9356b026
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-12-03 14:53:24 +0000

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const person = require("./variable")
 console.log(person)
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('one small W for man, one fat W for mankind'); //write a response to the client
+  res.write(`${person}: fbi wanted`); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
