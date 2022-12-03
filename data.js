cf5b8d4cd2a0523bc85a10a283d44945603ab9db
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-12-03 18:05:43 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,157 @@
+let data = {
+    item1 : {
+        name: "Skull Bath Bomb gift set",
+        description: "Black Oud, Vegan, Organic, dark, creepy, Self Care, Birthday Present, Custom Gift for Her",
+        price: "£3.00",
+        image: "image_5.jpg",
+        category: ["set", "bomb"]
+    },
+    item2 : {
+        name: "Save the Ocean Gift Set",
+        description: "Save the earth bath bomb Set, turtle bath bombs, Sweet Vanilla, Vegan, Self Care, Birthday Present, Custom Gift",
+        price: "£3.25",
+        image: "image_1.jpg",
+        category: ["set", "bomb", "bar"]
+    },
+    item3 : {
+        name: "Cauldron bath bomb and bracelet gift set",
+        description: "Glittery, Wizard gifts, Witch bracelets, Vegan, Spells, witchcraft, Custom Gift for her",
+        price: "£3.99",
+        image: "image_3.jpg",
+        category: ["set", "bomb"]
+    },
+    item4 : {
+        name: "Christmas bath bomb gift Set",
+        description: "REDUCED PLASTIC, Santa, christmas tree, Organic, Vegan, Self Care, Custom Gifts for her, Bath bombs for kids",
+        price: "£4.00",
+        image: "image_6.jpg",
+        category: ["set", "bomb"]
+    },
+    item5 : {
+        name: "Christmas bath bomb Gift Set 2",
+        description: "REDUCED PLASTIC, Christmas bath bombs, Organic, Vegan, Self Care, Custom Gifts for her, Bath bombs for kids",
+        price: "£3.50",
+        image: "image_7.jpg",
+        category: ["set", "bomb"]
+    },
+    item6 : {
+        name: "Enchanted elephant bath bombs Gift set",
+        description: "Joyous scented, Vegan, Self Care, Birthday Present, Custom Gift for Her, Bath Bomb Gift",
+        price: "£3.00",
+        image: "image_18.jpg",
+        category: ["set", "bomb"]
+    },
+    item7 : {
+        name: "Crystal surprise bath bomb",
+        description: "Diamond Dream- Joyous scented, Vegan, Self Care, Birthday Present, Custom Gift for Her, Bath Bomb Gift",
+        price: "£3.50",
+        image: "image_11.jpg",
+        category: ["bomb"]
+    },
+    item8 : {
+        name: "Kisses Of Fire Bath Bombs",
+        description: "Ice Pixie, Vegan, Organic, Summer, Self Care, Birthday Present, Custom Gift for Her",
+        price: "£3.60",
+        image: "image_10.jpg",
+        category: ["bomb"]
+    },
+    item9 : {
+        name: "Bath salts Gift Set",
+        description: "Rainbow, Ice Pixie, Sweet Vanilla, Raspberry Slush, Vegan, Summer, Self Care, Birthday Present, Custom Gift for Her",
+        price: "£4.40",
+        image: "image_15.jpg",
+        category: ["set", "bar"]
+    },
+    item10 : {
+        name: "4x Bath Bomb Gift Set",
+        description: "Ice Pixie, Sweet Vanilla, Raspberry Slush, Vegan, Organic, Summer, Self Care, Birthday Present, Custom Gift for Her",
+        price: "£12.50",
+        image: "image_8.jpg",
+        category: ["set", "bomb"]
+    },
+    item11 : {
+        name: "Spooky pumpkin bath bomb cauldrons",
+        description: "gift set, Colourful, Wizard gifts, Creepy, Scary, fun, Vegan, Spells, witchcraft, Custom Gift for her",
+        price: "£3.50",
+        image: "image_20.jpg",
+        category: ["bomb"]
+    },
+    item12 : {
+        name: "Kisses of fire bath rocks",
+        description: "Snow Fairy, Vegan, Organic, birthday present, custom gifts for her, boy, girl, gift, bath bombs, bath bomb",
+        price: "£3.00",
+        image: "image_14.jpg",
+        category: ["rocks"]
+    },
+    item13 : {
+        name: "Cosmic bath rocks",
+        description: "Raspberry Slush, Vegan, Organic, Summer, Self Care, Birthday Present, Custom Gifts for kids, Custom Gifts for her",
+        price: "£5.00",
+        image: "image_17.jpg",
+        category: ["rocks"]
+    },
+    item14 : {
+        name: "Crocodile bath bombs",
+        description: "Reduced plastic, Crocodile gift set, Raspberry Slush, Vegan, Self Care, Birthday Present, Custom Gift, Gifts for kids",
+        price: "£4.00",
+        image: "image_13.jpg",
+        category: ["bomb"]
+    },
+    item15 : {
+        name: "Turtle bath bombs",
+        description: "Reduced plastic, Gift set - Sweet Vanilla, Vegan gifts, Organic, Self Care, Birthday Present, Gifts for her",
+        price: "£4.25",
+        image: "image_12.jpg",
+        category: ["bomb"]
+    },
+    item16 : {
+        name: "What On Earth Bath Bombs",
+        description: "Black Oud, Vegan, Organic, Summer, Self Care, Birthday Present, Custom Gift for Her",
+        price: "£3.60",
+        image: "image_22.jpg",
+        category: ["bomb"]
+    },
+    item17 : {
+        name: "Lava bath rocks",
+        description: "stocking fillers, Raspberry Slush, Vegan, Organic, Self Care, Birthday Present, Gifts for kids, Custom Gifts for her",
+        price: "£3.20",
+        image: "image_16.jpg",
+        category: ["rocks"]
+    },
+    item18 : {
+        name: "Large Peach and Aubergine Bubble bars",
+        description: "Raspberry Slush, Vegan, Organic, Self Care, Birthday Present, Custom Gift for Her, Valentine's day",
+        price: "£5.50",
+        image: "image_26.jpg",
+        category: ["bar"]
+    },
+    item19 : {
+        name: "Crystal Lovers Gift Set",
+        description: "Crystal bath bombs, Crystal Soap, Rose quartz, Vegan, Self Care, Birthday Present, Custom Gift",
+        price: "£3.00",
+        image: "image_25.jpg",
+        category: ["set", "bomb", "bar", "rocks"]
+    },
+    item20 : {
+        name: "Cosmic Love Bath Bombs",
+        description: "Raspberry Slush, Vegan, Organic, Summer, Self Care, Birthday Present, Custom Gift for Her",
+        price: "£3.60",
+        image: "image_9.jpg",
+        category: ["bomb"]
+    },
+    item21 : {
+        name: "Dragon bath bombs",
+        description: "Black Oud, Vegan,Dungeon mystical, Organic, Summer, Self Care, Birthday Present, Custom Gift for kids",
+        price: "£4.00",
+        image: "image_21.jpg",
+        category: ["bomb"]
+    },
+    item22 : {
+        name: "Ocean Vibes Bath Bombs",
+        description: "Sweet Vanilla, Vegan, Organic, Summer, Self Care, Birthday Present, Custom Gift for Her",
+        price: "£3.60",
+        image: "image_23.jpg",
+        category: ["bomb"]
+    }
+}
+module.exports = { data }
--- index.js
+++ index.js
@@ -12,7 +12,107 @@ const fs = require("fs")
 //     })
 //   }
 // }).listen(80, () => console.log("server listening on port 80"))
+const { data } = require("./data.js")
+Object.keys(data).forEach((i, index) => {
+    server.get(`/item${index}`, (req, res) => {
+        res.json(data[i])
+    })
+    server.get(`/product${index}`, (req, res) => {
+        res.render('product', {
+            name: data[i].name,
+            image: data[i].image,
+            price: data[i].price,
+            category: data[i].category[0]
+        })
+    });
+})
 
+// ===== Image serving =====
+Object.keys(data).forEach((i) => {
+    let str = data[i].name
+    str = str.replace(/\s/g, '-')
+    server.get(`/product/${str}`, (req, res) =>{
+        res.sendFile(__dirname + `/serverImages/${data[i].image}`)
+    })
+})
+
+totalData = {total: 0, priority: 0, set: 0, bomb: 0, rocks: 0, bar: 0}
+totalData.total = Object.keys(data).length
+Object.keys(data).forEach(i =>{
+    data[i].category.forEach((i) => {
+        switch(i){
+            case "priority":
+                totalData.priority++
+                break
+            case "set":
+                totalData.set++
+                break
+            case "bomb":
+                totalData.bomb++
+                break
+            case "rocks":
+                totalData.rocks++
+                break
+            case "bar":
+                totalData.bar++
+                break
+        }
+    })
+})
+Object.keys(totalData).forEach(i =>{
+    if(totalData[i] == 0){
+        totalData[i] = null
+    }
+})
+
+server.get("/totals", (req, res) => {
+    res.json(totalData)
+})
+// Give data a position
+Object.keys(data).forEach((i) => {
+    data[i].position = [0, 0, 0, 0, 0]
+})
+
+// Set those positions
+function setPositions(filter, place){
+    let tempCount = 1
+    Object.keys(data).forEach((i, index) => {
+        data[i].category.forEach((event) => {
+            if(event == filter){
+                if(data[i].category.length > 0){
+                    data[i].position[place] = tempCount
+                    tempCount++
+                }
+            }
+        })
+    })
+}
+setPositions("priority", 0)
+setPositions("set", 1)
+setPositions("bomb", 2)
+setPositions("rocks", 3)
+setPositions("bar", 4)
+// console.log(data)
+
+function setRequests(filter){
+    let tempCount = 0
+    Object.keys(data).forEach((i, index) => {
+        data[i].category.forEach((event) => {
+            if(event == filter){
+                server.get(`/item/${filter}/${tempCount}`, (req, res) => {
+                    res.json(data[i])
+                })
+                tempCount++
+            }
+        })
+    })
+}
+setRequests("priority")
+setRequests("set")
+setRequests("bomb")
+setRequests("rocks")
+setRequests("bar")
+console.log(totalData)
 http.createServer((req, res) => {
   let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
   let extname = path.extname(filePath)
